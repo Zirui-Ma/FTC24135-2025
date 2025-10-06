14822bf4ac338da795c067c5a9cff7258a04ca77
Zirui-Ma <ziruima@outlook.com>
2025-10-06 11:48:32 -0600

    Added PinPoint Odometry 1006

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMovement.java
@@ -6,13 +6,19 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcontroller.external.samples.RobotHardware;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+
 
 /** Button Config for Drive
  * * Joy Right Y                : Drive
@@ -38,6 +44,7 @@ public class RobotMovement extends OpMode {
 
     private FtcDashboard ftcDashboard;
 
+    public GoBildaPinpointDriver odo;
 
     public void init() {
         // Initialize IMU from RobotHardware
@@ -50,94 +57,89 @@ public class RobotMovement extends OpMode {
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
         ftcDashboard = FtcDashboard.getInstance();
         telemetry = new MultipleTelemetry(telemetry, ftcDashboard.getTelemetry());
 
         aprilTag = new AprilTag_TEST(hardwareMap, ftcDashboard);
         aprilTag.init();
+
+        odo = hardwareMap.get(GoBildaPinpointDriver.class,"odo");
+
+        odo.setOffsets(-149.225, -165.1);
+        odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD);
+        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.REVERSED);
+        odo.resetPosAndIMU();
+        odo.setPosition(new Pose2D(DistanceUnit.METER,0,0,AngleUnit.DEGREES,0));
     }
 
 
     @SuppressLint("DefaultLocale")
     public void loop() {
+        //Update Odometry
+        odo.update();
+        //odo.setPosition()
+
         // Toggle control mode
 
         /** Reset IMU heading using button back and reset odometry
          * * need to remove this feature, as back button reseting imu may interfer with Roadrunner pose estimation.
          * * back button is using for retracting slide after auto phase and initial start of teleops
-
-         if (gamepad_1.getButton(BACK) || gamepad_2.getButton(BACK) && !backPressed) {
-         //robot.initIMU();
-         //robot.resetDriveEncoders();
-         debounceTimer.reset();
-         backPressed = true;
-         } else if (!gamepad_1.getButton(BACK) || !gamepad_2.getButton(BACK)) {
-         backPressed = false;
-         }
          */
 
-        double drive = 0.0;
-        double strafe = 0.0;
-        double rotate = 0.0;
 
-        // Mecanum drive calculations
-        setMecanumDrivePower(drive, strafe, rotate, powerFactor);
-        // Update telemetry with the latest data
-        // empty
+
+        double y = -gamepad1.right_stick_y; // Remember, Y stick value is reversed
+        double x = gamepad1.left_stick_x; // Counteract imperfect strafing
+        double rx = gamepad1.right_stick_x;
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+
+        double powerFactor = 0.5;
+
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator * powerFactor;
+        double backLeftPower = (y - x + rx) / denominator * powerFactor;
+        double frontRightPower = (y - x - rx) / denominator * powerFactor;
+        double backRightPower = (y + x - rx) / denominator * powerFactor;
+
+        frontLeftMotor.setPower(frontLeftPower);
+        backLeftMotor.setPower(backLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backRightMotor.setPower(backRightPower);
 
         aprilTag.tagProcessing();
         AprilTagDetection tagValue = aprilTag.tagUpdate();
 
         if (tagValue != null) {
-            telemetry.addData("Bearing", tagValue.ftcPose.bearing);
-            telemetry.addData("x", tagValue.ftcPose.x);
-            telemetry.addData("y", tagValue.ftcPose.y);
-            telemetry.addData("z", tagValue.ftcPose.z);
-            telemetry.addData("Roll", tagValue.ftcPose.roll);
-            telemetry.addData("Pitch", tagValue.ftcPose.pitch);
-            telemetry.addData("Yaw", tagValue.ftcPose.yaw);
-            telemetry.addData("Range", tagValue.ftcPose.range);
+            telemetry.addData("x", roundNumber(tagValue.ftcPose.x));
+            telemetry.addData("y", roundNumber(tagValue.ftcPose.y));
+            telemetry.addData("z", roundNumber(tagValue.ftcPose.z));
+            telemetry.addData("Bearing", roundNumber(tagValue.ftcPose.bearing));
+            telemetry.addData("Roll", roundNumber(tagValue.ftcPose.roll));
+            telemetry.addData("Pitch", roundNumber(tagValue.ftcPose.pitch));
+            telemetry.addData("Yaw", roundNumber(tagValue.ftcPose.yaw));
+            telemetry.addData("Range", roundNumber(tagValue.ftcPose.range));
         }
 
-    }// end of driveloop
-
-    double lerp(double current, double target, double accel_Slowness, double decel_Slowness) {
-        double alpha = (Math.abs(target) > Math.abs(current)) ? accel_Slowness : decel_Slowness;
-        return current + alpha * (target - current);
+        telemetry.addData("odometry x", roundNumber(odo.getPosX()));
+        telemetry.addData("odometry y", roundNumber(odo.getPosY()));
+        telemetry.addData("odometry heading", roundNumber(odo.getHeading()));
+        telemetry.update();
     }
 
-    private void setMecanumDrivePower(double drive, double strafe, double rotate, double powerFactor) {
-
-        // Mecanum wheel drive formula
-        double desiredFrontLeftPower = drive + strafe + rotate;
-        double desiredFrontRightPower = drive - strafe - rotate;
-        double desiredBackLeftPower = drive - strafe + rotate;
-        double desiredBackRightPower = drive + strafe - rotate;
-
-        // Constrain the power within +-1.0
-        double maxPower = Math.max(
-                Math.max(Math.abs(desiredFrontLeftPower), Math.abs(desiredFrontRightPower)),
-                Math.max(Math.abs(desiredBackRightPower), Math.abs(desiredBackLeftPower))
-        );
-
-        if (maxPower > 1.0) {
-            desiredFrontLeftPower /= maxPower;
-            desiredFrontRightPower /= maxPower;
-            desiredBackLeftPower /= maxPower;
-            desiredBackRightPower /= maxPower;
-        }
-
-        double frontLeftPower = lerp(frontLeftMotor.getPower(), desiredFrontLeftPower, 0.25, 0.5);
-        double frontRightPower = lerp(frontRightMotor.getPower(), desiredFrontRightPower, 0.25, 0.5);
-        double backLeftPower = lerp(backLeftMotor.getPower(), desiredBackLeftPower, 0.25, 0.5);
-        double backRightPower = lerp(backRightMotor.getPower(), desiredBackRightPower, 0.25, 0.5);
-
-
-        // Set motor powers
-        frontLeftMotor.setPower(Range.clip(frontLeftPower * powerFactor, -1.0, 1.0));
-        frontRightMotor.setPower(Range.clip(frontRightPower * powerFactor, -1.0, 1.0));
-        backLeftMotor.setPower(Range.clip(backLeftPower * powerFactor, -1.0, 1.0));
-        backRightMotor.setPower(Range.clip(backRightPower * powerFactor, -1.0, 1.0));
-
+    private double roundNumber(double number) {
+        return (double) Math.round(number * 100) / 100;
     }
 }
